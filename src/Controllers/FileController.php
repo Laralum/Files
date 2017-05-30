1789fa3591773002a8457a1a95097b2497642401
24aitor <contact@aitorriba.com>
2017-05-30 15:56:32 +0200

    Added Dropzone.js

--- src/Controllers/FileController.php
+++ src/Controllers/FileController.php
@@ -13,35 +13,12 @@ class FileController extends Controller
         return view('laralum_files::laralum.upload');
     }
 
-    public function save($request)
+    public function save(Request $request)
     {
-        User::first()->update([
-            'name' => 'eeeeeeeeeeeeeeegreeee'
-        ]);
-        // if (isset($_FILES['files'])) {
-        //     User::first()->update([
-        //         'name' => 'sireq'
-        //     ]);
-        //     for($i=0;$i<count($_FILES['files']['name']);$i++){
-        //             foreach($_FILES['files'] as $v=>$file) {
-        //                 $errors = array();
-        //                 $file_name = $_FILES['files']['name'][$i];
-        //                 \Laralum\Users\Model\User::first()->update([
-        //                     'name' => $file_name
-        //                 ]);
-        //                 $file_size = $_FILES['files']['size'][$i];
-        //                 $file_tmp = $_FILES['files']['tmp_name'][$i];
-        //                 $file_type = $_FILES['files']['type'][$i];
-        //                 $file_ext = strtolower(end(explode('.',$_FILES['files']['name'][$i])));
-        //             }
-        //         }
-        // } else {
-        //     User::first()->update([
-        //         'name' => 'noreq'
-        //     ]);
-        // }
-        // // if $request->files
-        // // return view('laralum_files::laralum.upload');
-        // return '200';
+        $file = $request->file('file');
+        $name = time().'-'.str_limit($file->getClientOriginalName(), 150, '');
+
+        $file->move('laralum/files', $name);
+        return "File uploaded";
     }
 }
--- src/Views/laralum/upload.blade.php
+++ src/Views/laralum/upload.blade.php
@@ -6,7 +6,7 @@
 @section('title', __('laralum_events::general.create_event'))
 @section('subtitle', __('laralum_events::general.create_event_desc'))
 @section('css')
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/dropzone.css">
 @endsection
 @section('breadcrumb')
     <ul class="uk-breadcrumb">
@@ -25,88 +25,8 @@
                         {{ __('laralum_events::general.create_event') }}
                     </div>
                     <div class="uk-card-body">
-                        <form class="uk-form-stacked" method="POST" action="{{ route('laralum::events.store') }}">
+                        <form class="dropzone" action="{{ route('laralum::files.save') }}" method="POST">
                             {{ csrf_field() }}
-                            <fieldset class="uk-fieldset">
-
-
-                                <div class="uk-margin">
-                                    <label class="uk-form-label">@lang('laralum_events::general.title')</label>
-                                    <div class="uk-form-controls">
-                                        <input value="{{ old('title') }}" name="title" class="uk-input" type="text" placeholder="@lang('laralum_events::general.title_ph')">
-                                    </div>
-                                </div>
-
-    <div class="test-upload uk-placeholder uk-text-center">
-        <span uk-icon="icon: cloud-upload"></span>
-        <span class="uk-text-middle">Attach binaries by dropping them here or</span>
-        <div uk-form-custom>
-            <input type="file" multiple>
-            <span class="uk-link">selecting one</span>
-        </div>
-    </div>
-
-    <progress id="progressbar" class="uk-progress" value="0" max="100" hidden></progress>
-
-    <script>
-    console.log($('meta[name="csrf-token"]').attr('content'));
-        (function ($) {
-
-            var bar = $("#progressbar")[0];
-
-            UIkit.upload('.test-upload', {
-
-                url: '',
-                multiple: true,
-                beforeSend: function(xhr) {
-                    xhr.setRequestHeader('X-CSRF-TOKEN', $('meta[name="csrf-token"]').attr('content'));
-                },
-                beforeAll: function() { console.log('beforeAll', arguments); },
-                load: function() { console.log('load', arguments); },
-                error: function() { console.log('error', arguments); },
-                complete: function() { console.log('complete', arguments); },
-
-                loadStart: function (e) {
-                    console.log('loadStart', arguments);
-
-                    bar.removeAttribute('hidden');
-                    bar.max =  e.total;
-                    bar.value =  e.loaded;
-                },
-
-                progress: function (e) {
-                    console.log('progress', arguments);
-                    bar.max =  e.total;
-                    bar.value =  e.loaded;
-                },
-
-                loadEnd: function (e) {
-                    console.log('loadEnd', arguments);
-                    bar.max =  e.total;
-                    bar.value =  e.loaded;
-                },
-
-                completeAll: function () {
-                    console.log('completeAll', arguments);
-                    setTimeout(function () {
-                        bar.setAttribute('hidden', 'hidden');
-                    }, 1000);
-
-                    alert('Upload Completed');
-                }
-            });
-
-        })(jQuery);
-
-    </script>
-
-
-                                <div class="uk-margin">
-                                    <button type="submit" class="uk-button uk-button-primary uk-align-right">
-                                        <span class="ion-forward"></span>&nbsp; {{ __('laralum_events::general.create_event') }}
-                                    </button>
-                                </div>
-                            </fieldset>
                         </form>
                     </div>
                 </div>
@@ -114,4 +34,7 @@
             <div class="uk-width-1-1@s uk-width-3-5@l"></div>
         </div>
     </div>
+@endsection
+@section('js')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.3.0/dropzone.js"></script>
 @endsection
